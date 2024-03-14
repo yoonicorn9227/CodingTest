d01873d2d31dc3688d3e8a8319e5f9d195fb06c2
yoonicorn9227 <143577001+yoonicorn9227@users.noreply.github.com>
2024-03-14 14:13:35 +0900

    [level 0] Title: 특수문자 출력하기, Time: 153.46 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+      
+        String str = "!@#$%^&*(\\'\"<>?:;";
+        System.out.print(str);
+        
+    }
+}
