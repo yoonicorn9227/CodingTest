7020d6df41a24473ba9500fa70dc02f4939e344a
yoonicorn9227 <143577001+yoonicorn9227@users.noreply.github.com>
2024-04-01 18:06:54 +0900

    [level 0] Title: 홀짝 구분하기, Time: 155.40 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        	if(n>=1 && n<=1000) {
+		
+		if(n%2!=1) {
+			System.out.println(n+" is even");
+		} else {
+			System.out.println(n+" is odd");
+		}
+		
+		
+	}
+    }
+}
