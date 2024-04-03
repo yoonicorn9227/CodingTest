2d140b04a5adc2ba97dedb8f9524eae2153bc85f
yoonicorn9227 <143577001+yoonicorn9227@users.noreply.github.com>
2024-04-03 17:33:13 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+          if(number%n==0 && number%m==0) {
+        	answer = 1;
+        } else if(number%n==0 && number%m!=0) {
+        	answer = 0;
+        	
+        }
+        return answer;
+    }
+}
