8f090a864ad32202d2ffdcb37a0e10fbe2b900ae
yoonicorn9227 <143577001+yoonicorn9227@users.noreply.github.com>
2024-04-15 15:26:56 +0900

    [level 0] Title: rny_string, Time: 0.20 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,11 @@
+class Solution {
+	public String solution(String rny_string) {
+		String answer = "";
+
+		rny_string = rny_string.replace("m", "rn");
+
+		System.out.println(rny_string);
+        answer  = rny_string;
+		return answer;
+	}// solution
+}// CLASS
